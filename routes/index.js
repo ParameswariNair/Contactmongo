2ed14b975b10b06a8e9643811c6dbdbf4eb4b2bd
ParameswariNair <parameswarisreekumar@gmail.com>
2018-11-23 10:24:23 +0530

    firstcommit

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Heros = require('../models/hero');
-
+var Heroes=require('../models/hero.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Super Heroes' });
-});
-
-router.get('/saveData', function(req, res, next) {
- Heros.saveNew(req.query)
-  .then(function(){
-  	res.redirect('/getAllHeros')
-  })
-   
-   .catch(console.log('ERR :: err in resolving the promise'))
-
-});
-
-router.get('/deleteRow', function(req, res, next) {
- Heros.deleteRow(req.query)
-  .then(function(){
-  	res.redirect('/getAllHeros')
-  })
-   
-   .catch(console.log('ERR :: err in resolving the promise'))
-
+  res.render('index', { title: 'My Application' });
 });
 
 router.get('/getAllHeros', function(req, res, next) {
-	Heros.getAll()
-	 .then(function(retVal){
-	 	res.render('heros',{data : retVal})
-	 })
-  .catch( console.log('ERR :: error in resolving the promise'))
+	Heroes.getAll()
+	.then(function(retVal){
+		console.log(retVal);
+		res.render('heros', {data :retVal})
+	})
+	.catch( console.log('ERR :: in resolving the promice'))
+});
+router.get('/saveData', function(req, res, next) {
+	Heroes.saveData(req.query)
+ 	.then(function(){
+ 		res.redirect('/getAllHeros')
+	})
+	.catch(console.log('ERR::in resolving the promise'))
 });
 
-router.get('/viewRow', function(req, res, next) {
- Heros.viewRow(req.query)
-  .then(function(retVal){
-  	 res.render('view',{data: retVal})
-  })
-   
-   .catch(console.log('ERR :: err in resolving the promise'))
-
+router.get('/deleteRow', function(req, res, next) {
+	Heroes.deleteRow(req.query)
+	.then(function(){
+		res.redirect('/getAllHeros')
+	})
+	.catch( console.log('ERR :: in resolving the promice'))
 });
-
-
 
 
 module.exports = router;
