6126074b6bc4ae518a81fb48389df543538f4241
ParameswariNair <parameswarisreekumar@gmail.com>
2018-11-24 09:15:20 +0530

    first commit

--- models/hero.js
+++ models/hero.js
@@ -1,90 +1,127 @@
 var express = require('express');
-var fs = require("fs");
+var fs = require('fs');
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
+const ObjectId = Schema.ObjectId;   
+ 
 const contact = new Schema({
-  id: ObjectId,
-  name: String,
-  phoneNo: String
-  
+   id: ObjectId,
+   name: String,
+   phoneNo: String    
 });
 
-const myModel= mongoose.model('contact',contact);  
-let Heroes= {}
+const MyModel = mongoose.model('contact', contact);
+let Heroes={}
+
+// get all heroes from the database
 
 Heroes.getAll = function(){
- return new Promise (function(resolve, reject){	
-   const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
-   console.log(connection);
-  myModel.find({},function(err, contact){   //displays the table rows
+  return new Promise (function(resolve, reject){
 
-  	if(err)
-  	{
-  		console.log(err);
-  		console.log('ERR :: fetching data from datbase.');
-  	}
-  	else
-  	{
-  		console.log(contact);
-  		//console.log(fields);
-  		resolve(contact);  
-  	}
-   });
-  });
-  }
+    //create the connection to database
 
-  Heroes.saveData = function(newHeroData){
- return new Promise (function(resolve, reject){ 
-   const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
-   console.log(connection);
-
-   var newUser= myModel({
-     name:`${newHeroData.name}`,
-     phoneNo: `${newHeroData.phoneNo}`
-   });
-
-
-   console.log('newUser' + newUser)
-  newUser.save({},function(err, contact){  
-
-    if(err)
-    {
-      console.log(err);
-      console.log('ERR :: fetching data from datbase.');
-      reject();
-    }
-    else
-    {
-      console.log(contact);
-      //console.log(fields);
-      resolve(contact);  
-    }
-   });
-  });
-  }
-
-  Heroes.deleteRow = function(newHeroData){
-  return new Promise(function(resolve,reject){
-    const connection =mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+    const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
     console.log(connection);
-    myModel.findOneAndRemove({name : `${newHeroData.name}`}, function(err){
-    if (err) {
-      console.log(err);
-      console.log('ERR :: fetching data from database.');
-      reject();
-    }
-    else {
-      //console.log(result);
-      console.log(contact);
-      resolve(contact);
-    }
-   
+    MyModel.find({},function(err,contact){
+       
+      if (err) {
+        console.log(err);
+        console.log('ERR :: fetching data from database.');
+        reject();
+      }
+      else {
+        console.log (contact);
+        resolve(contact);
+      }
+    });
 });
-});  
+}
 
+Heroes.updateData = function(updateData){
+    return new Promise (function(resolve,reject){
+
+        //create the connection to database
+
+        const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+        console.log (connection);
+        MyModel.findOneAndUpdate({_id: `${updateData._id}`}, { name: `${updateData.name}` , phoneNo: `${updateData.phoneNo}`}, function(err, con) {
+          if (err) {
+                console.log(err);
+                console.log('ERR :: fetching data from database..');
+                reject();
+            }
+            else {
+                //console.log(result);
+                console.log('con.......'+ con);
+                resolve(con);
+
+            }
+        });
+});        
+    
+}
+
+Heroes.saveData = function(newContact){
+    return new Promise (function(resolve,reject){
+    
+        const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+        console.log(connection);
+        var newCont = new MyModel({
+            name: `${newContact.name}`,
+            phoneNo: `${newContact.phoneNo}`
+        });
+        newCont.save({},function(err,contact){
+            if (err) {
+                console.log(err);
+                console.log('ERR :: Saving data into database..');
+                reject();
+            }
+            else {
+                console.log(contact);
+                resolve(contact);
+            }
+        });
+    });
+}
+
+Heroes.deleteRow = function(delContData){
+    return new Promise(function(resolve,reject){
+        const connection =mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+        console.log(connection);
+        MyModel.findOneAndRemove({name : `${delContData.name}`}, function(err){
+            if (err) {
+                console.log(err);
+                console.log('ERR :: fetching data from database.');
+                reject();
+            }
+            else {      
+                console.log(contact);
+                resolve(contact);
+            }   
+        });
+    });  
+}
+
+Heroes.viewData = function(viewData){
+    return new Promise (function(resolve, reject){
+
+    //create the connection to database
+
+    const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+    console.log(connection);
+    MyModel.find({_id : `${viewData._id}`},function(err,contact){
+       
+        if (err) {
+            console.log(err);
+            console.log('ERR :: fetching data from database.');
+            reject();
+        }
+        else {
+            console.log (contact);
+            resolve(contact);
+        }
+    });
+});
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,11 @@ router.get('/getAllHeros', function(req, res, next) {
 	Heroes.getAll()
 	.then(function(retVal){
 		console.log(retVal);
-		res.render('heros', {data :retVal})
+		res.render('heros', {data : retVal})
 	})
 	.catch( console.log('ERR :: in resolving the promice'))
 });
+
 router.get('/saveData', function(req, res, next) {
 	Heroes.saveData(req.query)
  	.then(function(){
@@ -29,6 +30,31 @@ router.get('/deleteRow', function(req, res, next) {
 	})
 	.catch( console.log('ERR :: in resolving the promice'))
 });
+
+router.get('/viewData', function(req, res, next) {
+	Heroes.viewData(req.query)
+	.then(function(retVal){
+		res.render('view',{data : retVal})
+	})
+	.catch( console.log('ERR :: in resolving the promice'))
+});
+
+router.get('/updateData', function(req, res, next) {
+	Heroes.updateData(req.query)
+	 .then(function(retVal){
+	 	res.redirect('/getAllHeros')
+	 })
+    .catch(console.log('ERR :: is resolving the promise'))
+});
+
+/*router.get('/getAllValues', function(req, res, next) {
+	Heroes.getAll()
+	 .then(function(retVal){
+	res.send(retVal)
+	})
+	.catch( console.log('ERR :: in resolving the promice'))
+});
+*/
 
 
 module.exports = router;
